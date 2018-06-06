4fe205ab496ef496f9343f8d3997532feab7d7a7
aga3134 <aga3134@gmail.com>
2018-06-06 18:08:42 +0800

    use webpack to convert scss to css

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@
 var path = require("path");
 var webpack = require("webpack");
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var extractCSSPlugin = new MiniCssExtractPlugin({
+	filename: "[name].css",
+	chunkFilename: "[id].css"
+});
 
 module.exports = {
 	mode: "production", // "production" | "development" | "none"
 	entry: {
-		main: "./src/js/main.js"
+		main: ["./src/js/main.js","./src/scss/main.scss"]
 	},
 	output:{
 		path: path.resolve(__dirname,'dist'),
@@ -23,18 +29,33 @@ module.exports = {
 				],
                 exclude: /node_modules/
 			},
-			{
+			{	//for vue component style
 				test: /\.scss$/,
+				include: [
+					path.resolve(__dirname, "src/vue")
+				],
 				use: ["style-loader","css-loader","sass-loader"]
 			},
 			{
 				test: /\.vue$/,
 				loader: "vue-loader"
-			}
+			},
+			{	//convert scss to css
+            	test: /\.scss$/,
+            	include: [
+					path.resolve(__dirname, "src/scss")
+				],
+            	use: [
+		          MiniCssExtractPlugin.loader,
+		          'css-loader',
+		          'sass-loader',
+		        ]
+            },
 		]
 	},
 	plugins:[
-		new VueLoaderPlugin()
+		new VueLoaderPlugin(),
+		extractCSSPlugin
 	],
 	devtool: "source-map"
 };
